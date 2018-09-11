7d8fddda19234c749c059b1061a27cb8db25c517
SimonStrands <36299106+SimonStrands@users.noreply.github.com>
2018-09-11 08:44:03 +0200

    Update 11 sep 08:43

--- Högby IF/javascript.js
+++ Högby IF/javascript.js
@@ -1,21 +1,19 @@
-//Byta Iframes
-//Startsida
-/*document.getElementById("startsidatable").addEventListener("click", changetostartsida);
-changetostartsida(){
-    document.getElementById("iframe").src = "startsida.html";
-}
+/*
+startsida               Halv
+Tävlingar(sist)         X
+Träning                 X
+Resultat                X
+Statestik               X
+Arrangemang             X
+Aktiva                  X
+Arkiv                   X
+Länkar                  X
+Sponsorer               X
+Gästbook                X
+Kontakt                 X
+*/
 
-//Tävlingar
-document.getElementById("tavlingtable").addEventListener("click", changetotavling);
-changetotavling(){
-    document.getElementById("iframe").src = "tavling.html";
-}
 
-//Träning
-document.getElementById("traningtable").addEventListener("click", changetotraning);
-changetotraning(){
-    
-}*/
 
 //Iframe saker
 function getDocHeight(doc) {
@@ -36,4 +34,111 @@ function setIframeHeight(id) {
     // IE opt. for bing/msn needs a bit added or scrollbar appears
     ifrm.style.height = getDocHeight( doc ) + 4 + "px";
     ifrm.style.visibility = 'visible';
+}
+
+//Byta Iframes
+document.getElementById("startsidatable").addEventListener("click", startSida);
+document.getElementById("tavlingtable").addEventListener("click", tavlingSida);
+document.getElementById("traningtable").addEventListener("click", traningSida);
+document.getElementById("resultattable").addEventListener("click", resultatSida);
+document.getElementById("statestiktable").addEventListener("click", statestikSida);
+document.getElementById("arrangemangtable").addEventListener("click", arrangemangSida);
+document.getElementById("aktivatable").addEventListener("click", aktivaSida);
+document.getElementById("arkivtable").addEventListener("click", arkivSida);
+document.getElementById("lankartable").addEventListener("click", lankarSida);
+document.getElementById("sponsorertable").addEventListener("click", sponsorerSida);
+document.getElementById("gastbooktable").addEventListener("click", gastbookSida);
+document.getElementById("kontakttable").addEventListener("click", kontaktSida);
+
+//Startsida
+function startSida()
+{
+    
+    document.getElementById("iframe").src = "startsida.html";
+    
+}
+
+//Tävlingar
+function tavlingSida()
+{
+
+    document.getElementById("iframe").src = "tavling.html";
+    
+}
+
+//Träning
+function traningSida()
+{
+
+    document.getElementById("iframe").src = "traning.html";
+    
+}
+
+//resultat
+function resultatSida()
+{
+
+    document.getElementById("iframe").src = "resultat.html";
+    
+}
+
+//statestik
+function statestikSida()
+{
+
+    document.getElementById("iframe").src = "statestik.html";
+    
+}
+
+//arragemang
+function arrangemangSida()
+{
+
+    document.getElementById("iframe").src = "arrangemang.html";
+    
+}
+
+//aktiv
+function aktivaSida()
+{
+
+    document.getElementById("iframe").src = "aktiva.html";
+    
+}
+
+//arkiv
+function arkivSida()
+{
+
+    document.getElementById("iframe").src = "arkiv.html";
+    
+}
+
+
+function lankarSida()
+{
+
+    document.getElementById("iframe").src = "lankar.html";
+    
+}
+
+function sponsorerSida()
+{
+
+    document.getElementById("iframe").src = "sponsorer.html";
+    
+}
+
+function gastbookSida()
+{
+
+    document.getElementById("iframe").src = "gastbook.html";
+    
+}
+
+function kontaktSida()
+{
+
+    document.getElementById("iframe").src = "kontakt.html";
+    
 }
